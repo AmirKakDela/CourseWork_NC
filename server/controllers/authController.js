f6ad5f9ba4662c02b0fe136ded79fbc06e47d40b
Amir Akhmetzhanov <tawer321@bk.ru>
2021-11-27 17:41:15 +0400

    add authroutes on server

---
+++ server/controllers/authController.js
@@ -0,0 +1,43 @@
+const {validationResult} = require("express-validator");
+const User = require("../models/User");
+
+class authController {
+    async register(req, res) {
+        try {
+            console.log(req.body)
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) return res.status(400).json({message: 'Ошибка при регистрации', errors})
+
+            const {email, password, name} = req.body;
+            const candidate = await User.findOne({email});
+            if (candidate) return res.status(400).json({message: `Пользователь с почтовым адрессом ${email} уже существует.`})
+
+            const user = new User({email, password, name});
+            // await user.save();
+            return res.json({message: 'Пользователь создан'})
+        } catch (e) {
+            console.log(e);
+            res.send({message: 'Ошибка сервера при регистрации'});
+        }
+    }
+
+    async login(req, res) {
+        try {
+            const {email, password} = req.body;
+
+            const user = await User.findOne({email});
+            if (!user) return res.status(403).json({message: `Пользователя с таким почтовым адресом не существует.`});
+
+            if (user.password !== password) return res.status(403).json({message: 'Неверный пароль'});
+
+            const token = user._id + '_' + user.admin;
+            return res.json({token});
+
+        } catch (e) {
+            console.log(e);
+            res.send({message: 'Ошибка сервера при логине'});
+        }
+    }
+}
+
+module.exports = new authController();
---
+++ server/index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require('express');
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRouter = require('./routes/auth.router')
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/auth', authRouter);
+
+
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+    } catch (e) {
+        console.log(`Error before start`)
+    }
+}
+
+start();
+
