582636b1c4f9135e39cba1bf9acfdf61e6bd5193
Amir Akhmetzhanov <tawer321@bk.ru>
2021-12-04 18:09:45 +0400

    add album component

---
+++ music-app/src/components/Album/Album.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './album.css';
+
+
+const Album: React.FC = () => {
+    return (
+        <div className="album">
+            <img src="https://the-flow.ru/uploads/images/catalog/element/5de03395535b5.jpg" alt="cover"
+                 className="album__img"/>
+            <h2 className="album__name">
+                ОПГ Сити
+            </h2>
+            <h3 className="album__artist">
+                OG Buda
+            </h3>
+        </div>
+    );
+};
+
+export default Album;
