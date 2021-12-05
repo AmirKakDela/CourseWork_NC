b4e3f7a555f241e4b2b3c44aefbf6dbf256d45a6
Amir Akhmetzhanov <tawer321@bk.ru>
2021-12-05 20:07:16 +0400

    add artist card

---
+++ music-app/src/components/ArtistCard/ArtistCard.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './artictCard.css';
+import {ArtistType} from "../../types";
+import {Link} from "react-router-dom";
+
+type PropsType = {
+    artist: ArtistType
+}
+
+const ArtistCard: React.FC<PropsType> = (props) => {
+    return (
+        <Link to={`/artist/${props.artist.name.toLowerCase()}`}>
+            <div className="artist">
+                <img
+                    src={props.artist.image}
+                    alt="" className="artist__img"/>
+                <h1 className="artist__name">{props.artist.name}</h1>
+                <h4 className="artist__role">исполнитель</h4>
+            </div>
+        </Link>
+    );
+};
+
+export default ArtistCard;
