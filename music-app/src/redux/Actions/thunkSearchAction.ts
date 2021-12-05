970e327258271570f191e746d1396987e58a999e
Amir Akhmetzhanov <tawer321@bk.ru>
2021-12-05 20:11:29 +0400

    create redux search state

---
+++ music-app/src/redux/Actions/thunkSearchAction.ts
@@ -0,0 +1,24 @@
+import {SearchActionType, setSearchError, setSearchResult} from "./searchActions";
+import {Dispatch} from "redux";
+import axios from "axios";
+import {url} from "../../config";
+import {ErrorType} from "../../types";
+
+export const getSearchResult = (queryValue: string) => {
+    return async (dispatch: Dispatch<SearchActionType>) => {
+        try {
+            const response = await axios.get(`${url}/api/search/?query=${queryValue.trim()}`, {
+                headers: {
+                    Authorization: '' + localStorage.getItem('token')
+                }
+            });
+            if (!Object.keys(response.data).length) {
+                return dispatch(setSearchResult({songs: [], artists: []}));
+            }
+            dispatch(setSearchResult(response.data));
+        } catch (e) {
+            const u = e as ErrorType
+            dispatch(setSearchError(u.response.data.message))
+        }
+    }
+}
