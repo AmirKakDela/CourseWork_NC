a6c7859c47812e7981179068834b071ababdd897
Amir Akhmetzhanov <tawer321@bk.ru>
2021-12-05 21:29:10 +0400

    fix try catch

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -4,14 +4,19 @@ const Song = require("../models/Song");
 class searchController {
 
     async getSearchResult(req, res) {
-        const {query} = req.query;
-        if (query === '') return res.json({});
+        try {
+            const {query} = req.query;
+            if (query === '') return res.json({songs: [], artists: []});
 
-        const songs = await Song.find({name: {$regex: query, $options: "i"}}).limit(5);
-        const artists = await Artist.find({name: {$regex: query,  $options: "i"}}).limit(5);
-        // Todo: Здесь еще потом написать поиск по плейлистам
+            const songs = await Song.find({name: {$regex: query, $options: "i"}}).limit(5);
+            const artists = await Artist.find({name: {$regex: query, $options: "i"}}).limit(5);
+            // Todo: Здесь еще потом написать поиск по плейлистам
 
-        res.json({songs, artists});
+            res.json({songs, artists});
+        } catch (e) {
+            res.status(412).send({message: 'Возникла ошибка при поиске на стороне сервера'});
+            console.log('ошибка getSearchResult', e)
+        }
     }
 
 }
