3d477fa454465773f7de1ff0b29f76edfa01baee
Alyona <alyonagerasimova@gmail.com>
2021-12-21 16:28:13 +0400

    add album router

---
+++ server/controllers/AlbumController.js
@@ -0,0 +1,15 @@
+const Song = require("../models/Song");
+const User = require("../models/User");
+const {validationResult} = require("express-validator");
+const Artist = require("../models/Artist");
+
+class AlbumController {
+    getArtistAlbum(req,res){
+        try{
+
+        }catch (e){
+
+        }
+    }
+
+}
