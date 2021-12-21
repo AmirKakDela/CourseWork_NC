a32bc2c7911cabc8fe9535f2bfa431fd8713ee09
Alyona <alyonagerasimova@gmail.com>
2021-12-21 16:28:13 +0400

    add album router

--- server/index.js
+++ server/index.js
@@ -6,19 +6,20 @@ const authRouter = require('./routes/auth.router');
 const songRouter = require('./routes/song.router');
 const artistRouter = require('./routes/artist.router');
 const searchRouter = require('./routes/search.router');
-
+const albumRouter = require('./routes/album.router');
+const authMiddleware = require("../middlewares/auth.middleware");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(authMiddleware);
 app.use('/api/auth', authRouter);
 app.use('/api/song', songRouter);
 app.use('/api/artist', artistRouter);
 app.use('/api/search', searchRouter);
-
-
+app.use('/api/album', albumRouter);
 
 const start = async () => {
     try {
@@ -33,4 +34,3 @@ const start = async () => {
 }
 
 start();
-
