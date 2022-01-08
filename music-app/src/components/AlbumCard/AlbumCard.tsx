d81363474eb42d700e1dace25cc2550aca4505be
Alyona <alyonagerasimova@gmail.com>
2022-01-08 04:37:11 +0400

    done main page content

--- music-app/src/components/AlbumCard/AlbumCard.tsx
+++ music-app/src/components/AlbumCard/AlbumCard.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import './albumCard.scss';
+import React from "react";
+import "./albumCard.scss";
 
 const defaultImageUrl = "https://the-flow.ru/uploads/images/catalog/element/5de03395535b5.jpg";
 
-function AlbumCard ({ album, onAlbumClick }: any){
+function AlbumCard({ album, onAlbumClick }: any) {
     return (
         <div className="album" onClick={() => onAlbumClick(album._id)}>
             <img src={album.cover || defaultImageUrl} alt="cover"
@@ -14,6 +14,11 @@ function AlbumCard ({ album, onAlbumClick }: any){
             <h3 className="album__artist">
                 {album.artist}
             </h3>
+            <button className="album__btn">
+                <svg height="16" role="img" width="16" viewBox="0 -3 24 24" aria-hidden="true">
+                    <polygon points="21.57 12 5.98 3 5.98 21 21.57 12" fill="currentColor"/>
+                </svg>
+            </button>
         </div>
     );
 }
