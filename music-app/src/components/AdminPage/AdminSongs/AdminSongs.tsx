64280d3cf075d6b5ce6dc3e035bf12c551e0a5b6
Amir Akhmetzhanov <tawer321@bk.ru>
2022-02-25 16:00:07 +0400

    Mini fix

--- music-app/src/components/AdminPage/AdminSongs/AdminSongs.tsx
+++ music-app/src/components/AdminPage/AdminSongs/AdminSongs.tsx
@@ -65,7 +65,7 @@ const AdminSongs: React.FC = () => {
                             </Link>
                         </div>
                     })}
-                </div>
+                </>
             }
         </div>
     );
